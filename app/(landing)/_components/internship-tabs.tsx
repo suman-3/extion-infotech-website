07fc0c3597c7cb011c5b317786e58117305a3229
Suman Mondal <mondalsumanrock20@gmail.com>
2024-08-04 02:07:42 +0530

    chore: Update internship tabs component and landing page content

--- app/(landing)/_components/internship-tabs.tsx
+++ app/(landing)/_components/internship-tabs.tsx
@@ -27,9 +27,29 @@ export function InternShipsTabs() {
   ];
 
   return (
-    <div className="h-[47rem] md:h-[30rem] [perspective:1000px] relative  flex flex-col max-w-6xl mx-auto w-full items-start justify-start mb-8 lg:mb-12">
-      <Tabs tabs={tabs} />
-    </div>
+    <section className="w-full h-full flex flex-col gap-5">
+      <div className="flex flex-col space-y-5 items-center justify-center mt-5 md:mt-0 md:mb-8">
+        <h1 className="text-4xl md:text-5xl font-bold">
+          Streamline your business with our
+          <span className="md:ml-2 bg-black dark:bg-white text-background inline-block px-3 py-1 md:py-2 rounded-md">
+            Services
+          </span>
+        </h1>
+
+        <p className="text-md font-semibold md:text-xl text-left md:text-center w-full md:w-[60%]">
+          At Extion Infotech, our dedication to excellence shines through our
+          meticulously crafted IT solutions. From innovative software
+          development to comprehensive cybersecurity measures, we deliver robust
+          and reliable services. In addition to our exceptional IT offerings,
+          Extion Infotech is a nurturing ground for aspiring professionals.
+          Discover our dynamic internship opportunities, designed to ignite your
+          passion for technology and provide valuable hands-on experience.
+        </p>
+      </div>
+      <div className="h-[47rem] md:h-[30rem] [perspective:1000px] relative  flex flex-col max-w-6xl mx-auto w-full items-start justify-start mb-8 lg:mb-12">
+        <Tabs tabs={tabs} />
+      </div>
+    </section>
   );
 }
 const Core = () => {
